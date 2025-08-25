ec5f1cee5d50f442162491ab9540f38578f7477d
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2025-08-25 22:28:43 +0200

    simplify

--- flux-bootstrap/flux-bootstrap.sh
+++ flux-bootstrap/flux-bootstrap.sh
@@ -5,7 +5,6 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 # Seconds
 short=5
-# long=900
 
 configmap=flux-bootstrap
 
@@ -23,12 +22,13 @@ error() {
 
 bootstrap() {
   NS=flux-system
-  BRANCH=$(kairos-agent config get "flux.git.branch")
-  REPO_URL=$(kairos-agent config get "flux.git.url")
-  PATH_IN_REPO="$(kairos-agent config get "flux.git.path")" # TODO replace with the cluster id
+  REPO_URL=$(kairos-agent config get "flux.url")
+  BRANCH=$(kairos-agent config get "flux.branch")
+  PATH_IN_REPO="$(kairos-agent config get "flux.path")" # TODO replace with the cluster id
   KEY=$(kairos-agent config get "flux.syncKeyFile")
+  COMPONENTS_EXTRA=$(kairos-agent config get "flux.components-extra")
 
-  flux install --namespace "$NS" --components-extra=image-reflector-controller,image-automation-controller
+  flux install --namespace "$NS" --components-extra="$COMPONENTS_EXTRA"
 
   kubectl -n "$NS" rollout status deploy/source-controller --timeout=180s
   kubectl -n "$NS" rollout status deploy/kustomize-controller --timeout=180s
@@ -52,49 +52,6 @@ if flux version &>/dev/null; then
   exit 0
 fi
 
-# Determine what VCS we need to bootstrap
-# Starting in kairos  2.8.15, kairos-agent command returns empty string  instead of "null"
-for vcs in bitbucket_server git github gitlab; do
-  # kairos-agent used to return `null` when a key wasn't set, now returns empty.
-  # we handle both
-  value=$(kairos-agent config get flux.$vcs 2>/dev/null)
-  if [[ $value != "null" && -n $value ]]; then
-    version_control=$vcs
-    break
-  fi
-done
-
-if [[ "${version_control}x" == "x" ]]; then
-  error "Unable to determine what version control provider to use, exiting..."
-  exit 1
-fi
-
-# Get flux envs and settings for our VCS
-mapfile -t envs < <(kairos-agent config get "flux.env" 2>/dev/null)
-mapfile -t args < <(kairos-agent config get "flux.$version_control" 2>/dev/null)
-declare -a cmdline
-
-for setting in "${envs[@]}"; do
-  if [[ $setting != "null" ]] && [[ $setting != "" ]]; then
-    env=$(echo "$setting" | cut -d: -f1)
-    value=$(echo "$setting" |  sed -n 's/^[^:]*: *//p')
-    if [[ "${value}x" != "x" ]]; then
-      export "$env"="$value"
-    fi
-  fi
-done
-
-# Set commandline args
-for setting in "${args[@]}"; do
-  if [[ $setting != "null" ]] && [[ $setting != "" ]]; then
-    arg=$(echo "$setting" | cut -d: -f1)
-    value=$(echo "$setting" |  sed -n 's/^[^:]*: *//p')
-    if [[ "${value}x" != "x" ]]; then
-      cmdline+=("--$arg" "$value")
-    fi 
-  fi
-done
-
 # Try to bootstrap Flux for 30 minutes, sleep 15 seconds between attempts
 minutes=30
 sleep=15
@@ -102,43 +59,39 @@ retry_attempt=1
 total_attempts=$(( minutes * 60 / sleep ))
 active="false"
 
-if [[ "${#cmdline[@]}" -eq 0 ]]; then
-  info "Flux was not configured in cloud-config, not bootstrapping"
-  exit 0
-else
-  while [[ $retry_attempt -le $total_attempts ]]; do
-    if [[ "$active" != "true" ]]; then
-      # Ensure only one host tries to bootstrap, whichever makes the configmap first
-      if ! timeout $short kubectl version &> /dev/null; then
-        info "Kubernetes API not ready yet, sleeping"
-      else
-        if ! timeout $short kubectl create configmap $configmap --from-literal=hostname="$(hostname)"; then
-          warn "Unable to create configmap, another node may be active"
-        fi
-
-        # The configmap exists but we must finally check if the hostname matches
-        if [[ "$(timeout $short kubectl get configmap -n default $configmap -o jsonpath='{.data.hostname}')" != "$(hostname)" ]]; then
-          error "Flux bootstrap ConfigMap exists but another node is active, exiting..."
-          exit 3
-        fi
-
-        # We must be the active node
-        active="true"
+while [[ $retry_attempt -le $total_attempts ]]; do
+  if [[ "$active" != "true" ]]; then
+    # Ensure only one host tries to bootstrap, whichever makes the configmap first
+    if ! timeout $short kubectl version &> /dev/null; then
+      info "Kubernetes API not ready yet, sleeping"
+    else
+      if ! timeout $short kubectl create configmap $configmap --from-literal=hostname="$(hostname)"; then
+        warn "Unable to create configmap, another node may be active"
       fi
-    fi
-    
-    if [[ "$active" == "true" ]]; then
-      if bootstrap; then
-        cleanup
-        exit 0
-      fi
-    fi
 
-    warn "Install attempt $retry_attempt (of $total_attempts) failed, retrying in $sleep seconds"
-    (( retry_attempt = retry_attempt + 1 ))
-    sleep $sleep
-  done
-fi
+      # The configmap exists but we must finally check if the hostname matches
+      if [[ "$(timeout $short kubectl get configmap -n default $configmap -o jsonpath='{.data.hostname}')" != "$(hostname)" ]]; then
+        error "Flux bootstrap ConfigMap exists but another node is active, exiting..."
+        exit 3
+      fi
+
+      # We must be the active node
+      active="true"
+    fi
+  fi
+  
+  if [[ "$active" == "true" ]]; then
+    if bootstrap; then
+      cleanup
+      exit 0
+    fi
+  fi
+
+  warn "Install attempt $retry_attempt (of $total_attempts) failed, retrying in $sleep seconds"
+  (( retry_attempt = retry_attempt + 1 ))
+  sleep $sleep
+done
+
 
 error "Failed to bootstrap with Flux, timed out ($minutes minutes)"
 exit 4
